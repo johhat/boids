414cc02fe6db5759de6c2e1c5289994ca0613016
Johan <johan.hatleskog@gmail.com>
2015-06-10 21:42:34 +0200

    Fixed typo

--- ui-components/ui.js
+++ ui-components/ui.js
@@ -1,7 +1,7 @@
 ﻿'use strict';
 /* global $: true */
 /* global animation: true */
-/* global boidWheights: true */
+/* global boidWeights: true */
 
 //Slider for selecting initial number of boids
 //---------------------------------------------
@@ -25,65 +25,65 @@ $('#slider1').slider({
     min: 0,
     max: 20,
     step: 0.1,
-    value: boidWheights.separation
+    value: boidWeights.separation
 });
 
-$('#slider1val').text(boidWheights.separation);
+$('#slider1val').text(boidWeights.separation);
 
 $('#slider1').on('slide', function (slideEvt) {
     $('#slider1val').text(slideEvt.value);
-    boidWheights.separation = slideEvt.value;
+    boidWeights.separation = slideEvt.value;
 });
 
 $('#slider2').slider({
     min: 0,
     max: 20,
     step: 0.1,
-    value: boidWheights.alginment
+    value: boidWeights.alginment
 });
 $('#slider2').on('slide', function (slideEvt) {
 
 $('#slider2val').text(boidWheights.alginment);
     $('#slider2val').text(slideEvt.value);
-    boidWheights.alginment = slideEvt.value;
+    boidWeights.alginment = slideEvt.value;
 });
 
 $('#slider3').slider({
     min: 0,
     max: 20,
     step: 0.1,
-    value: boidWheights.cohesion
+    value: boidWeights.cohesion
 });
-$('#slider3val').text(boidWheights.cohesion);
+$('#slider3val').text(boidWeights.cohesion);
 
 $('#slider3').on('slide', function (slideEvt) {
     $('#slider3val').text(slideEvt.value);
-    boidWheights.cohesion = slideEvt.value;
+    boidWeights.cohesion = slideEvt.value;
 });
 
 $('#slider4').slider({
     min: 0,
     max: 20,
     step: 0.1,
-    value: boidWheights.obstacle
+    value: boidheights.obstacle
 });
-$('#slider4val').text(boidWheights.obstacle);
+$('#slider4val').text(boidWeights.obstacle);
 
 $('#slider4').on('slide', function (slideEvt) {
     $('#slider4val').text(slideEvt.value);
-    boidWheights.obstacle = slideEvt.value;
+    boidWeights.obstacle = slideEvt.value;
 });
 
 $('#slider5').slider({
     min: 0,
     max: 20,
     step: 0.1,
-    value: boidWheights.predators
+    value: boidWeights.predators
 });
 
-$('#slider5val').text(boidWheights.predators);
+$('#slider5val').text(boidWeights.predators);
 
 $('#slider5').on('slide', function (slideEvt) {
     $('#slider5val').text(slideEvt.value);
-    boidWheights.predators = slideEvt.value;
+    boidWeights.predators = slideEvt.value;
 });
